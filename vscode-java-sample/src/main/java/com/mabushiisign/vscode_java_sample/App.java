640c635ac4f97344291b47b2cc8fb481c2b927eb
mabushiisign <mabushii.sign@gmail.com>
2020-12-10 21:50:16 +0900

    テストコード生成

--- vscode-java-sample/src/main/java/com/mabushiisign/vscode_java_sample/App.java
+++ vscode-java-sample/src/main/java/com/mabushiisign/vscode_java_sample/App.java
@@ -4,10 +4,12 @@ package com.mabushiisign.vscode_java_sample;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
